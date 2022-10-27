c394afbea0e310b93e2728683f5e7080eaf34cef
Ryuichi Ueda <ryuichiueda@gmail.com>
2022-10-27 12:36:40 +0900

    Delete yamada

--- test.bash
+++ test.bash
@@ -1,3 +1,26 @@
 #!/bin/bash
+# SPDX-...
+# SPDX-...
 
-seq 5 | ./plus
+ng () {
+	echo ${1}行目が違うよ
+	res=1
+}
+
+res=0
+
+### I/O TEST ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "${LINENO}"
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng "${LINENO}"
+[ "${out}" = "" ] || ng "${LINENO}"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng "${LINENO}"
+[ "${out}" = "" ] || ng "${LINENO}"
+
+[ "${res}" = 0 ] && echo OK
+exit $res
